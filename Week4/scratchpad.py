2eef6015c3c1ce79b3b2b1b5c3f1391d00291355
Celaric, Michael (mc7628) <mc7628@att.com>
2020-08-24 10:16:11 -0500

    Working  reconcile_countries_by_code, I think

--- Week4/scratchpad.py
+++ Week4/scratchpad.py
@@ -55,58 +55,59 @@ def reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries):
     
     converter = build_country_code_converter(codeinfo)
     
-    print("plot_countries - ", plot_countries)
-    print("gdp_countries - ", gdp_countries)
-    print("converter - ", converter, "\n")
+#     print("plot_countries - ", plot_countries)
+#     print("converter - ", converter)
+#     print("gdp_countries - ", gdp_countries,"\n")
+    
+
+    # Do all key conversions to lower case   
+#     plot_countries_lower = dict((key1.lower(), val1) for key1, val1 in plot_countries.items())  
+#     converter_lower = dict((key2.lower(), val2) for key2, val2 in converter.items())
+#     gdp_countries_lower = dict((key3.lower(), val3) for key3, val3 in gdp_countries.items()) 
+#       
+#     print("plot_countries keys - ", plot_countries)
+#     print("plot_keys_lower - ", plot_countries_lower, "\n")
+#     print("converter keys - ", converter)
+#     print("converter_lower - ", converter_lower, "\n")
+#     print("gdp_countries keys - ", gdp_countries)
+#     print("gdp_countries_lower - ", gdp_countries_lower, "\n")
+
+#     # THIS CODE BLOCK WORKS, PASSES OWLTEST, DO NOT ALTER!!!
+#     for country in plot_countries:
+#         for code in converter:
+#             if country == code.lower() and converter[code] in gdp_countries:
+#                 found_countries[country] = converter[code]
+#  
+#     for country in plot_countries:
+#         if country not in found_countries:
+#             not_found.add(country)
 
     for country in plot_countries:
         for code in converter:
-            if country == code.lower() and converter[code] in gdp_countries.keys():
-                found_countries[country] = converter[code]
- 
+            if country.lower() == code.lower():       
+                for gdp in gdp_countries:
+                    if gdp.lower() == converter[code].lower():
+                        found_countries[country] = gdp
+
     for country in plot_countries:
         if country not in found_countries:
             not_found.add(country)
-# 
-#     return found_countries, not_found
-    
-    # Do all key conversions to lower case    
-    plot_keys_lower = [x.lower() for x in plot_countries]
-    converter_keys_lower = [y.lower() for y in converter]
-    gdp_keys_lower = [z.lower() for z in gdp_countries]
-    
-#     print("plot_countries keys - ", plot_countries.keys())
-#     print("plot_keys_lower - ", plot_keys_lower, "\n")
-#     print("converter keys - ", converter.keys())
-#     print("converter_keys_lower - ", converter_keys_lower)
-#     print("gdp_countries keys - ", gdp_countries.keys())
-#     print("gdp_keys_lower - ", gdp_keys_lower)
-       
-#     for code in plot_countries:
-#         print(code)
-#         if code in plot_keys_lower:
-#             print("plot country code is lower case - ", code)
-#             print(plot_keys_lower)
-#             for country in gdp_countries:
-#                 if country.lower() in gdp_keys_lower and country.lower() in gdp_keys_lower:
-#                     print("code - ", code, "country - ", country)
 
     return found_countries, not_found
-
-
 
 
 ## TESTS
 ## PASSES FROM
 ## https://www.coursera.org/learn/python-visualization/discussions/weeks/4/threads/w6HhDlUARVeh4Q5VADVXlQ?sort=createdAtAsc&page=1
-# codeinfo = {'codefile': 'code4.csv', 'separator': ',', 'quote': '"', 'plot_codes': 'ISO3166-1-Alpha-2', 'data_codes': 'ISO3166-1-Alpha-3'}
-# plot_countries = {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}
-# gdp_countries =  {'USA': {'Country Name': 'United States', 'Country Code': 'USA'}, 'NOR': {'Country Name': 'Norway', 'Country Code': 'NOR'}}
-#    
-# print("Expected:  ({'no': 'NOR', 'us': 'USA'}, {'pr'})  ")
-# print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
+codeinfo = {'codefile': 'code4.csv', 'separator': ',', 'quote': '"', 'plot_codes': 'ISO3166-1-Alpha-2', 'data_codes': 'ISO3166-1-Alpha-3'}
+plot_countries = {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}
+gdp_countries =  {'USA': {'Country Name': 'United States', 'Country Code': 'USA'}, 'NOR': {'Country Name': 'Norway', 'Country Code': 'NOR'}}
+     
 
-print(reconcile_countries_by_code({'quote': '"', 'codefile': 'code4.csv', 'plot_codes': 'ISO3166-1-Alpha-2', 'separator': ',', 'data_codes': 'ISO3166-1-Alpha-3'}, {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}, {'NOR': {'Country Code': 'NOR', 'Country Name': 'Norway'}, 'PRI': {'Country Code': 'PRI', 'Country Name': 'Puerto Rico'}, 'USA': {'Country Code': 'USA', 'Country Name': 'United States'}}) ) 
+print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
+print("({'no': 'NOR', 'us': 'USA'}, {'pr'}) - Expected\n")
+
+# print(reconcile_countries_by_code({'quote': '"', 'codefile': 'code4.csv', 'plot_codes': 'ISO3166-1-Alpha-2', 'separator': ',', 'data_codes': 'ISO3166-1-Alpha-3'}, {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}, {'NOR': {'Country Code': 'NOR', 'Country Name': 'Norway'}, 'PRI': {'Country Code': 'PRI', 'Country Name': 'Puerto Rico'}, 'USA': {'Country Code': 'USA', 'Country Name': 'United States'}}) ) 
 
 """
 For an overview of the function, see here:
@@ -127,39 +128,45 @@ print out every dictionary, including
 (3) the various 'lower' converter dictionaries that you make.
 Having these at hand will make debugging much easier.
 """
-"""
-# Example 1 plot_countries lc:UC, code _converter UC:UC
+
+## Example 1 plot_countries lc:UC, code _converter UC:UC
+## PASS
+print("Example 1 plot_countries lc:UC, code _converter UC:UC")
 codeinfo = {'codefile': 'code4.csv', 'plot_codes': 'ISO3166-1-Alpha-2',
             'data_codes': 'ISO3166-1-Alpha-3', 'quote': '"', 'separator': ','}
 plot_countries = {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}
 gdp_countries =  {'USA': {'Country Name': 'United States', 'Country Code': 'USA'},
                   'PRI': {'Country Name': 'Puerto Rico', 'Country Code': 'PRI'},
                   'NOR': {'Country Name': 'Norway', 'Country Code': 'NOR'}}
-
+  
 print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("Expected:   ({'pr': 'PRI', 'no': 'NOR', 'us': 'USA'}, set())")
+print("({'pr': 'PRI', 'no': 'NOR', 'us': 'USA'}, set()) - Expected\n")
 
 # Example 2 plot_countries UC:lc, code _converter lc:UC
+print("Example 2 plot_countries UC:lc, code _converter lc:UC")
 codeinfo  =  {'separator': ',', 'quote': "'", 'plot_codes': 'Cd2',
               'data_codes': 'Cd3', 'codefile': 'code2.csv'}
 plot_countries = {'C2': 'c2', 'C5': 'c5', 'C4': 'c4', 'C3': 'c3', 'C1': 'c1'}
 gdp_countries = {'ABC': {'Country Name': 'Country1', 'Code': 'ABC', '2000': '1', '2001': '2', '2002': '3', '2003': '4', '2004': '5', '2005': '6'},
                 'GHI': {'Country Name': 'Country2', 'Code': 'GHI', '2000': '10', '2001': '11', '2002': '12', '2003': '13', '2004': '14', '2005': '15'}}
-
+ 
 print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("Expected:  ({'C3': 'GHI', 'C1': 'ABC'}, {'C5', 'C2', 'C4'})")
+print("({'C3': 'GHI', 'C1': 'ABC'}, {'C5', 'C2', 'C4'}) - Expected\n")
 
 
 # Example 3 plot_countries lc:UC, code _converter UC:UC, no countries in gdp_countries
+## PASS...?
+print("Example 3 plot_countries lc:UC, code _converter UC:UC, no countries in gdp_countries")
 codeinfo = {'quote': '"', 'data_codes': 'ISO3166-1-Alpha-3', 
             'plot_codes': 'ISO3166-1-Alpha-2', 'separator': ',', 'codefile': 'code4.csv'}
 plot_countries = {'jp': 'Japan', 'cn': 'China', 'ru': 'Russian Federation'}
 gdp_countries = {}
-
+ 
 print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("Expected: ({}, {'jp', 'cn', 'ru'})")
+print("({}, {'jp', 'cn', 'ru'}) - Expected\n")
 
 # Example 4 plot countries UC:lc, code_converter lc:MixED
+print("Example 4 plot countries UC:lc, code_converter lc:MixED")
 codeinfo =  {'quote': "'", 'separator': ',', 'plot_codes': 'Code4', 'codefile': 'code1.csv', 'data_codes': 'Code3'}
 plot_countries =  {'C4': 'c4', 'C3': 'c3', 'C2': 'c2', 'C1': 'c1', 'C5': 'c5'}
 gdp_countries =  {
@@ -170,7 +177,7 @@ gdp_countries =  {
 }
 
 print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("Expected ({'C4': 'ef', 'C3': 'Kl', 'C2': 'qR', 'C1': 'WX'}, {'C5'})")
+print("({'C4': 'ef', 'C3': 'Kl', 'C2': 'qR', 'C1': 'WX'}, {'C5'}) - Expected ")
 """
-
+"""
 
