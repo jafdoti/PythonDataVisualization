7ae20f706155da282cf59691bf9ae648857e7edf
Celaric, Michael (mc7628) <mc7628@att.com>
2020-08-26 06:23:35 -0500

    Final project submitted code
    Updated reconcile_countries_by_code, added build_map_dict_by_code

--- Week4/scratchpad.py
+++ Week4/scratchpad.py
@@ -54,33 +54,6 @@ def reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries):
     not_found = set()
     
     converter = build_country_code_converter(codeinfo)
-    
-#     print("plot_countries - ", plot_countries)
-#     print("converter - ", converter)
-#     print("gdp_countries - ", gdp_countries,"\n")
-    
-
-    # Do all key conversions to lower case   
-#     plot_countries_lower = dict((key1.lower(), val1) for key1, val1 in plot_countries.items())  
-#     converter_lower = dict((key2.lower(), val2) for key2, val2 in converter.items())
-#     gdp_countries_lower = dict((key3.lower(), val3) for key3, val3 in gdp_countries.items()) 
-#       
-#     print("plot_countries keys - ", plot_countries)
-#     print("plot_keys_lower - ", plot_countries_lower, "\n")
-#     print("converter keys - ", converter)
-#     print("converter_lower - ", converter_lower, "\n")
-#     print("gdp_countries keys - ", gdp_countries)
-#     print("gdp_countries_lower - ", gdp_countries_lower, "\n")
-
-#     # THIS CODE BLOCK WORKS, PASSES OWLTEST, DO NOT ALTER!!!
-#     for country in plot_countries:
-#         for code in converter:
-#             if country == code.lower() and converter[code] in gdp_countries:
-#                 found_countries[country] = converter[code]
-#  
-#     for country in plot_countries:
-#         if country not in found_countries:
-#             not_found.add(country)
 
     for country in plot_countries:
         for code in converter:
@@ -96,88 +69,110 @@ def reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries):
     return found_countries, not_found
 
 
-## TESTS
-## PASSES FROM
-## https://www.coursera.org/learn/python-visualization/discussions/weeks/4/threads/w6HhDlUARVeh4Q5VADVXlQ?sort=createdAtAsc&page=1
-codeinfo = {'codefile': 'code4.csv', 'separator': ',', 'quote': '"', 'plot_codes': 'ISO3166-1-Alpha-2', 'data_codes': 'ISO3166-1-Alpha-3'}
-plot_countries = {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}
-gdp_countries =  {'USA': {'Country Name': 'United States', 'Country Code': 'USA'}, 'NOR': {'Country Name': 'Norway', 'Country Code': 'NOR'}}
-     
+def build_map_dict_by_code(gdpinfo, codeinfo, plot_countries, year):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      codeinfo       - A country code information dictionary
+      plot_countries - Dictionary mapping plot library country codes to country names
+      year           - String year for which to create GDP mapping
 
-print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("({'no': 'NOR', 'us': 'USA'}, {'pr'}) - Expected\n")
+    Output:
+      A tuple containing a dictionary and two sets.  The dictionary
+      maps country codes from plot_countries to the log (base 10) of
+      the GDP value for that country in the specified year.  The first
+      set contains the country codes from plot_countries that were not
+      found in the GDP data file.  The second set contains the country
+      codes from plot_countries that were found in the GDP data file, but
+      have no GDP data for the specified year.
+    """
 
-# print(reconcile_countries_by_code({'quote': '"', 'codefile': 'code4.csv', 'plot_codes': 'ISO3166-1-Alpha-2', 'separator': ',', 'data_codes': 'ISO3166-1-Alpha-3'}, {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}, {'NOR': {'Country Code': 'NOR', 'Country Name': 'Norway'}, 'PRI': {'Country Code': 'PRI', 'Country Name': 'Puerto Rico'}, 'USA': {'Country Code': 'USA', 'Country Name': 'United States'}}) ) 
+#  let reconcile_by_code return a dictionary plot_countries:gdp country codes, and a set, out_set_1
+#     go through the countries in that returned dictionary
+#         
+#         for those having data for the year you need in the country:data dictionary
+#                 add data to output dictionary
+#         the others, add to second output set
+#             
+#     return output dictionary, output set1, outoutput set2
+    countries = {}
+    not_found = set()
+    missing = set()
+    
+#  read in appropriate gdp file as a nested dict of country:data    
+    with open(gdpinfo.get("gdpfile"), 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=gdpinfo.get("separator"), 
+                                quotechar=gdpinfo.get("quote"))
+        for data in reader:
+            for country in plot_countries:
+                if plot_countries.get(country) in data.values():
+                    try:
+                        countries[country] = math.log10(float(data.get(year)))
+                    except ValueError:
+                        missing.add(country)
+                        continue
+                    
+    # We found countries that have gdp data or missing data
+    # Now build the set of missing/not found countries
+    for country in plot_countries:
+        if country not in countries and country not in missing:
+            not_found.add(country)
 
-"""
-For an overview of the function, see here:
-http://py3.codeskulptor.org/#user301_eEwf449Gi1_0.py
-
-Below are four examples for reconcile_countries_by_code()
-      lc = lowercase, UC = uppercase
-One at a time, copy and paste each example block, 
-including the two associated print() expressions,
-somewhere below your reconcile_countries_by_code() function, then run your module.
-If all four produce "Expected" output, you should be ready for OwlTest 
-... and for Problem 3.
-
-Please Note: When developing this function, you will find it helpful to 
-print out every dictionary, including 
-(1) those you are given by OwlTest, 
-(2) the 'converter' dictionary that you get from build_country_code_converter(), and 
-(3) the various 'lower' converter dictionaries that you make.
-Having these at hand will make debugging much easier.
-"""
-
-## Example 1 plot_countries lc:UC, code _converter UC:UC
-## PASS
-print("Example 1 plot_countries lc:UC, code _converter UC:UC")
-codeinfo = {'codefile': 'code4.csv', 'plot_codes': 'ISO3166-1-Alpha-2',
-            'data_codes': 'ISO3166-1-Alpha-3', 'quote': '"', 'separator': ','}
-plot_countries = {'pr': 'Puerto Rico', 'no': 'Norway', 'us': 'United States'}
-gdp_countries =  {'USA': {'Country Name': 'United States', 'Country Code': 'USA'},
-                  'PRI': {'Country Name': 'Puerto Rico', 'Country Code': 'PRI'},
-                  'NOR': {'Country Name': 'Norway', 'Country Code': 'NOR'}}
-  
-print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("({'pr': 'PRI', 'no': 'NOR', 'us': 'USA'}, set()) - Expected\n")
-
-# Example 2 plot_countries UC:lc, code _converter lc:UC
-print("Example 2 plot_countries UC:lc, code _converter lc:UC")
-codeinfo  =  {'separator': ',', 'quote': "'", 'plot_codes': 'Cd2',
-              'data_codes': 'Cd3', 'codefile': 'code2.csv'}
-plot_countries = {'C2': 'c2', 'C5': 'c5', 'C4': 'c4', 'C3': 'c3', 'C1': 'c1'}
-gdp_countries = {'ABC': {'Country Name': 'Country1', 'Code': 'ABC', '2000': '1', '2001': '2', '2002': '3', '2003': '4', '2004': '5', '2005': '6'},
-                'GHI': {'Country Name': 'Country2', 'Code': 'GHI', '2000': '10', '2001': '11', '2002': '12', '2003': '13', '2004': '14', '2005': '15'}}
- 
-print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("({'C3': 'GHI', 'C1': 'ABC'}, {'C5', 'C2', 'C4'}) - Expected\n")
+    return {}, set(), set()
 
 
-# Example 3 plot_countries lc:UC, code _converter UC:UC, no countries in gdp_countries
-## PASS...?
-print("Example 3 plot_countries lc:UC, code _converter UC:UC, no countries in gdp_countries")
-codeinfo = {'quote': '"', 'data_codes': 'ISO3166-1-Alpha-3', 
-            'plot_codes': 'ISO3166-1-Alpha-2', 'separator': ',', 'codefile': 'code4.csv'}
-plot_countries = {'jp': 'Japan', 'cn': 'China', 'ru': 'Russian Federation'}
-gdp_countries = {}
- 
-print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("({}, {'jp', 'cn', 'ru'}) - Expected\n")
+def render_world_map(gdpinfo, codeinfo, plot_countries, year, map_file):
+    """
+    Inputs:
+      gdpinfo        - A GDP information dictionary
+      codeinfo       - A country code information dictionary
+      plot_countries - Dictionary mapping plot library country codes to country names
+      year           - String year of data
+      map_file       - String that is the output map file name
 
-# Example 4 plot countries UC:lc, code_converter lc:MixED
-print("Example 4 plot countries UC:lc, code_converter lc:MixED")
-codeinfo =  {'quote': "'", 'separator': ',', 'plot_codes': 'Code4', 'codefile': 'code1.csv', 'data_codes': 'Code3'}
-plot_countries =  {'C4': 'c4', 'C3': 'c3', 'C2': 'c2', 'C1': 'c1', 'C5': 'c5'}
-gdp_countries =  {
-'qR': {'ID': 'A 5 ', 'CC': 'qR'},
-'Kl': {'ID': 'B 6', 'CC': 'Kl'},
-'WX': {'ID': 'C 7 ', 'CC': 'WX'},
-'ef': {'ID': 'D 8', 'CC': 'ef'}
-}
+    Output:
+      Returns None.
 
-print(reconcile_countries_by_code(codeinfo, plot_countries, gdp_countries))
-print("({'C4': 'ef', 'C3': 'Kl', 'C2': 'qR', 'C1': 'WX'}, {'C5'}) - Expected ")
-"""
-"""
+    Action:
+      Creates a world map plot of the GDP data in gdp_mapping and outputs
+      it to a file named by svg_filename.
+    """
+    return
 
+
+def test_render_world_map():
+    """
+    Test the project code for several years
+    """
+    gdpinfo = {
+        "gdpfile": "isp_gdp.csv",
+        "separator": ",",
+        "quote": '"',
+        "min_year": 1960,
+        "max_year": 2015,
+        "country_name": "Country Name",
+        "country_code": "Country Code"
+    }
+
+    codeinfo = {
+        "codefile": "isp_country_codes.csv",
+        "separator": ",",
+        "quote": '"',
+        "plot_codes": "ISO3166-1-Alpha-2",
+        "data_codes": "ISO3166-1-Alpha-3"
+    }
+
+    # Get pygal country code map
+    pygal_countries = pygal.maps.world.COUNTRIES
+
+    # 1960
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "1960", "isp_gdp_world_code_1960.svg")
+
+    # 1980
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "1980", "isp_gdp_world_code_1980.svg")
+
+    # 2000
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "2000", "isp_gdp_world_code_2000.svg")
+
+    # 2010
+    render_world_map(gdpinfo, codeinfo, pygal_countries, "2010", "isp_gdp_world_code_2010.svg")
